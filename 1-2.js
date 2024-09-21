e9c8403775e4fcbcb5f99ce94acb4a350b31b2a7
Beastoloch <54348523+Beastoloch@users.noreply.github.com>
2024-09-21 19:32:47 +0700

    Create 1-2.js

---
+++ 1-2.js
@@ -0,0 +1,17 @@
+class Animal {
+    speak() {
+        return "Animal speaks";
+    }
+}
+
+class Dog extends Animal {
+    speak() {
+        return "Woof!";
+    }
+}
+
+class Cat extends Animal {
+    speak() {
+        return "Meow!";
+    }
+}
